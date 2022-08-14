d948e9a34520712872bf5091549f20413ca35433
Raoul Wograndl <Wograndl.Raoul@outlook.com>
2022-08-14 15:20:39 +0200

    Fix client logging macros

--- Hazel/src/Hazel/Log.h
+++ Hazel/src/Hazel/Log.h
@@ -35,8 +35,8 @@ namespace Hazel {
 #define HZ_CORE_FATAL(...) ::Hazel::Log::GetCoreLogger()->fatal(__VA_ARGS__)
 
 // Client logging macros
-#define HZ_CORE_TRACE(...) ::Hazel::Log::GetClientLogger()->trace(__VA_ARGS__)
-#define HZ_CORE_INFO(...) ::Hazel::Log::GetClientLogger()->info(__VA_ARGS__)
-#define HZ_CORE_WARN(...) ::Hazel::Log::GetClientLogger()->warn(__VA_ARGS__)
-#define HZ_CORE_ERROR(...) ::Hazel::Log::GetClientLogger()->error(__VA_ARGS__)
-#define HZ_CORE_FATAL(...) ::Hazel::Log::GetClientLogger()->fatal(__VA_ARGS__)
+#define HZ_CLIENT_TRACE(...) ::Hazel::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define HZ_CLIENT_INFO(...) ::Hazel::Log::GetClientLogger()->info(__VA_ARGS__)
+#define HZ_CLIENT_WARN(...) ::Hazel::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define HZ_CLIENT_ERROR(...) ::Hazel::Log::GetClientLogger()->error(__VA_ARGS__)
+#define HZ_CLIENT_FATAL(...) ::Hazel::Log::GetClientLogger()->fatal(__VA_ARGS__)
