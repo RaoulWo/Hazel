9b0c9ec7a461c7526239fb24662bf5f65d6ab9fd
Raoul Wograndl <Wograndl.Raoul@outlook.com>
2022-08-20 15:28:46 +0200

    Implement basic layer system

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -16,19 +16,39 @@ namespace Hazel {
 
 	}
 
+	void Application::Run()
+	{
+		while (m_Running)
+		{
+			for (Layer* layer : m_LayerStack)
+				layer->OnUpdate();
+
+			m_Window->OnUpdate();
+		}
+	}
+
+	void Application::PushLayer(Layer* layer)
+	{
+		m_LayerStack.PushLayer(layer);
+	}
+
+	void Application::PushOverlay(Layer* overlay)
+	{
+		m_LayerStack.PushOverlay(overlay);
+	}
+
 	void Application::OnEvent(Event& e)
 	{
 		EventDispatcher dispatcher(e);
 		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
 
 		HZ_CORE_INFO("{0}", e);
-	}
 
-	void Application::Run()
-	{
-		while (m_Running)
+		for (std::vector<Layer*>::iterator it = m_LayerStack.end(); it != m_LayerStack.begin(); )
 		{
-			m_Window->OnUpdate();
+			(*--it)->OnEvent(e);
+			/*if (e.Handled)
+				break;*/
 		}
 	}
 
